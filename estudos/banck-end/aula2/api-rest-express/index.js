60767b7fc21175d4ed3a4398d90b56a4c5a02792
igorspestana <igor@igor-win11>
2022-11-17 16:54:52 -0300

    api rest com objeto router

--- estudos/banck-end/aula2/api-rest-express/index.js
+++ estudos/banck-end/aula2/api-rest-express/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
-const app = express();
+const app = express()
 const router = express.Router();
 
 app.use(express.json());
@@ -13,65 +13,80 @@ const db = [
         title: 'hello1',
         body: 'hello2 world body blablabla',
         creat_at: new Date(),
-        update_at: new Date(), 
+        update_at: new Date(),
     },
     {
         id: 2,
         title: 'hello2',
         body: 'hello2 world body blablabla',
         creat_at: new Date(),
-        update_at: new Date(), 
+        update_at: new Date(),
     },
     {
         id: 3,
         title: 'hello3',
         body: 'hello3 world body blablabla',
         creat_at: new Date(),
-        update_at: new Date(), 
+        update_at: new Date(),
     }
 ]
 
-app.get('/posts', function (req, res) {
-    res.send(db)
-})
-app.post('/posts', function(req, res) {
-    db.push(req.body);
+router
+    .route('/posts')
+    .all(function (req, res, next) {
+        console.log(new Date)
+        console.log(req.headers)
+        next()
+    })
+    .get(function (req, res) {
+        res.send(db)
+    })
+    .post(function (req, res) {
+        db.push(req.body);
 
-    res.status(201);
-    res.json(req.body)
+        res.status(201);
+        res.json(req.body)
+    })
 
-    
-app.get('/posts/:id', function(req, res) {
-    const { id } = req.params;
-    const ret = db.find( e => e.id == id);
+router
+    .param('id', function (req, res, next) {
+        console.log(new Date)
+        console.log(req.headers)
+        next()
+    })
+    .route('/posts/:id')
+    .get(function (req, res) {
+        const { id } = req.params;
+        const ret = db.find(e => e.id == id);
 
-    if (!ret) return res.status(404).json();
+        if (!ret) return res.status(404).json();
 
-    res.status(201);
-    res.json(ret);
-});
-});
-app.put('/posts/:id', function(req, res) {
-    const { id } = req.params;
-    const ret = db.find( e => e.id == id);
+        res.status(201);
+        res.json(ret);
+    })
+    .put(function (req, res) {
+        const { id } = req.params;
+        const ret = db.find(e => e.id == id);
 
-    if (!ret) return res.status(404).json();
-    
-    const { title } = req.body;
+        if (!ret) return res.status(404).json();
 
-    ret.title = title;
+        const { title } = req.body;
 
-    res.status(201);
-    res.json(ret);
-});
-app.delete('/posts/:id', function(req, res) {
-    const { id } = req.params;
-    const ret = db.filter( e => e.id != id);
-    
-    res.status(200);
-    res.json(ret);
-});
+        ret.title = title;
 
-app.listen(3000, function() {
+        res.status(201);
+        res.json(ret);
+    })
+    .delete(function (req, res) {
+        const { id } = req.params;
+        const ret = db.filter(e => e.id != id);
+
+        res.status(200);
+        res.json(ret);
+    })
+
+app.use('', router)
+
+app.listen(3000, function () {
     console.log('Server listen on http://localhost:3000')
 })
