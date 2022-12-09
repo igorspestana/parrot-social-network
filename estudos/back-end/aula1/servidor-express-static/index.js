9db5e0b52bd782309bf063af5e6567816a1f3cca
Igor Pestana <igor@pop-os.localdomain>
2022-12-09 17:43:37 -0300

    diretórios atualizados

--- estudos/back-end/aula1/servidor-express-static/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/', express.static(__dirname + "/"))
-
-app.listen(4000, () => {
-    console.log('server listen on http://localhost:4000')
-});
